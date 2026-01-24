f6df890126851a7174200b723e4a755c1fc67e3f
Faria Sultana <fs1231@g.rit.edu>
2026-01-23 21:21:26 -0500

    Added database api and key

---
+++ createClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  "https://tpnduwecssoydxsygwar.supabase.co",
+  "sb_publishable_17p_-uyaWvWE9S2boiz6pA_0HMxhTUA"
+)
