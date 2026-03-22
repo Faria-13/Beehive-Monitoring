53a7605aef9fb7a69c776484e352853e7de07bc2
Faria Sultana <fs1231@g.rit.edu>
2026-03-21 20:32:37 -0400

    plugged in real sensor ID for temperature

--- beehive/src/pages/Temperature.jsx
+++ beehive/src/pages/Temperature.jsx
@@ -15,7 +15,7 @@ export default function Temperature() {
   return (
     <Box sx={{ px: 2, py: 2, maxWidth: 1400, mx: "auto" }}>
       <SensorDashboardRow
-        selectedSensorId={41}
+        selectedSensorId={4}
         sensorIds={sensorIds}
         sensorOptions={sensorOptions}
         showLocalWeather={true}
